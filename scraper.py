6044425a7ad3aeee6fbe35e3f4808cad9328fd38
Anthony Lopez <Antl21292@gmail.com>
2023-05-13 20:26:27 -0400

    first function working

--- scraper.py
+++ scraper.py
@@ -3,7 +3,21 @@ from playwright.sync_api import sync_playwright
 
 
 def get_citations_needed_count(url_string):
-    pass
+    """
+     Gets the number of citations needed on the webpage
+     :param url_string:
+     :return: An integer of the citations needed
+     """
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(slow_mo=500)
+        page = browser.new_page()
+        page.goto(url_string)
+
+        soup = bs(page.content(), 'html.parser')
+        citations_result = soup.find_all(title="Wikipedia:Citation needed")
+        print(f"There are {len(citations_result)} citations needed!")
+        browser.close()
+        return len(citations_result)
 
 
 def get_citations_needed_report(url_string):
