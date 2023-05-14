8f5271fab0029c10de4dcc9fb26ee8d764dfab56
Anthony Lopez <Antl21292@gmail.com>
2023-05-13 20:37:36 -0400

    cleaning up code

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ from playwright.sync_api import sync_playwright
 
 def get_citations_needed_count(url_string):
     """
-     Gets the number of citations needed on the webpage
+     Gets the number of citations needed on the webpage.
      :param url_string:
      :return: An integer of the citations needed
      """
@@ -21,23 +21,28 @@ def get_citations_needed_count(url_string):
 
 
 def get_citations_needed_report(url_string):
-    def get_citations_needed_report(url_string):
-        with sync_playwright() as playwright:
-            browser = playwright.chromium.launch(slow_mo=500)
-            page = browser.new_page()
-            page.goto(url_string)
+    """
+    Returns a report of the paragraphs that contain a "citation needed" link.
+    :param url_string:
+    :return: A report string
+    """
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(slow_mo=500)
+        page = browser.new_page()
+        page.goto(url_string)
 
-            soup = bs(page.content(), 'html.parser')
-            citations_result = soup.find_all(title="Wikipedia:Citation needed")
-            report = "Citation Report\n\n"
-            for citation in citations_result:
-                parent = citation.parent.parent.parent
-                report += parent.text + "\n"
+        soup = bs(page.content(), 'html.parser')
+        citations_result = soup.find_all(title="Wikipedia:Citation needed")
 
-            print(report)
-            # print(f"There are {len(citations_result)} citations needed!")
-            browser.close()
-            return report
+        report = "Citation Report:\n\n"
+
+        for citation in citations_result:
+            p_parent = citation.parent.parent.parent
+            report += p_parent.text + "\n"
+
+        print(report)
+        browser.close()
+        return report
 
 
 if __name__ == "__main__":
