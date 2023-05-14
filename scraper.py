d083748896191bd92417bad97e2e41b3fcfad6dd
Anthony Lopez <Antl21292@gmail.com>
2023-05-13 20:25:46 -0400

    created functions and boilerplate

--- scraper.py
+++ scraper.py
@@ -1,2 +1,16 @@
 from bs4 import BeautifulSoup as bs
 from playwright.sync_api import sync_playwright
+
+
+def get_citations_needed_count(url_string):
+    pass
+
+
+def get_citations_needed_report(url_string):
+    pass
+
+
+if __name__ == "__main__":
+    get_citations_needed_count("https://en.wikipedia.org/wiki/History_of_Mexico")
+    print("\n")
+    get_citations_needed_report("https://en.wikipedia.org/wiki/History_of_Mexico")
